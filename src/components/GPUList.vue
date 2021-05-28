8d22f57261a326256b4a123a45045b29229832b7
Ffinnis <potapovroman03@gmail.com>
2021-05-28 21:09:33 +0900

    shop has been added

---
+++ src/components/GPUList.vue
@@ -0,0 +1,43 @@
+<template>
+    <ul class="gpu-list">
+        <li v-for="gpu in gpuArray" :key="gpu.id" class="gpu-item">
+            <h2>
+                {{gpu.title}}
+            </h2>
+            <p>
+                Хэш-рейт: {{gpu.hash}}
+            </p>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "GPUList",
+        data() {
+            return{
+                gpuArray: [
+                    {
+                        id: 0,
+                        title: 'RX 480',
+                        hash: 0.2
+                    },
+                    {
+                        id: 1,
+                        title: 'RX 580',
+                        hash: 0.3
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    ul{
+        padding: 0;
+    }
+li{
+    list-style-type: none;
+}
+</style>
--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -2,11 +2,14 @@
     <h1>
         PC Shop
     </h1>
+   <GPUList />
 </template>
 
 <script>
+    import GPUList from "../components/GPUList";
     export default {
-        name: "Shop"
+        name: "Shop",
+        components: {GPUList},
     }
 </script>
 
